0ac8ef3220a0a2e38a53aa065b37647ebd572153
АМ <test@test.com>
2020-01-02 12:48:39 +0300

    обработка лова рыбы (скрытия и перекраски поплавка при клике по нему

--- fishingPlay.js
+++ fishingPlay.js
@@ -14,6 +14,7 @@ let goFloat = {
     width: 40,
     height: 40,
     color: 'black',
+    bitingColor: 'red',
     field: null
 };
 
@@ -45,8 +46,7 @@ function init() {
         goCast.width,
         goCast.height);
 
-    canvas.onclick = clickFloat;
-    canvas.onclick = clickCast;
+    canvas.onclick = allClicks;
     setInterval(play, 1000 / 50);
 }
 // класс определяющий параметры игрового прямоугольника и метод для его отрисовки
@@ -75,7 +75,8 @@ function clickFloat(poE){
         poE.y > goFloat.field.y &&
         poE.y <= (goFloat.field.y + goFloat.field.height)
     ){
-        goFloat.field.x += 20;
+        gbShowFloat = false;
+        goFloat.field.color = goFloat.color;
     }
 }
 function clickCast(poE){
@@ -86,13 +87,12 @@ function clickCast(poE){
         poE.y <= (goCast.field.y + goCast.field.height)
     ){
         gbShowFloat = true;
-        console.log(randn_bm());
         setTimeout(biting, randn_bm() * 3000);
     }
 }
 // Нормальное распеределение от 0 до 1 с МО 0.5
 function randn_bm() {
-    var u = 0, v = 0;
+    let u = 0, v = 0;
     while(u === 0) u = Math.random();
     while(v === 0) v = Math.random();
     let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
@@ -102,5 +102,12 @@ function randn_bm() {
 }
 // событие клевание рыбы
 function biting(){
-    goFloat.field.color = 'red';
+    if (gbShowFloat){ // без этого условия будет перекрашиваться даже не скрытый поплавок
+        goFloat.field.color = goFloat.bitingColor;
+    }
+}
+// вызывает все клики
+function allClicks(poE){
+    clickFloat(poE);
+    clickCast(poE);
 }
