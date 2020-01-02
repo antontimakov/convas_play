17223d26f4848899494b6aa4c831549f0d8cccc3
АМ <test@test.com>
2020-01-02 12:23:50 +0300

    клёв через случайный промежуток времени в среднем 1.5 секунды. Не больше 3

--- fishingPlay.js
+++ fishingPlay.js
@@ -86,16 +86,17 @@ function clickCast(poE){
         poE.y <= (goCast.field.y + goCast.field.height)
     ){
         gbShowFloat = true;
-        let timerId = setTimeout(biting, 3000);
+        console.log(randn_bm());
+        setTimeout(biting, randn_bm() * 3000);
     }
 }
-// Нормальное распеределение от 0 до 1 с МО близким к 0
+// Нормальное распеределение от 0 до 1 с МО 0.5
 function randn_bm() {
     var u = 0, v = 0;
     while(u === 0) u = Math.random();
     while(v === 0) v = Math.random();
     let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-    num = num / 5.0;
+    num = num / 10.0 + 0.5;
     if (num > 1 || num < 0) return randn_bm();
     return num;
 }
