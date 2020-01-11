e2180fc10579d834fe230f788b9db747c58b6d90
АМ <test@test.com>
2020-01-11 19:22:41 +0300

    доработка класса мсг

--- classes/Msg.js
+++ classes/Msg.js
@@ -4,6 +4,8 @@ class Msg {
         this.text = '';
         this.time = null;
         this.deley = 1500;
+        this.x = 372;
+        this.y = 100;
     }
     draw(){
         if (this.time && this.time > new Date().getTime()){
@@ -12,7 +14,7 @@ class Msg {
             goGame.context.textAlign = 'center';
             goGame.context.textBaseline = 'top';
             goGame.context.fillStyle = 'white';
-            goGame.context.fillText(this.text,  372, 100);
+            goGame.context.fillText(this.text,  this.x, this.y);
         }
     }
     add(poText){
