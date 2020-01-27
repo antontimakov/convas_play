3b24f733bb3da19e6c50a92c128cab075032d0c8
АМ <test@test.com>
2020-01-27 23:10:27 +0300

    добавление тестового предмета в магазине

--- classes/ShopWindow.js
+++ classes/ShopWindow.js
@@ -12,11 +12,18 @@ class ShopWindow{
             y: 0,
             width: 40,
             height: 40
-        }
+        };
+        this.item = {
+            x: 50,
+            y: 20,
+            width: 100,
+            height: 150
+        };
     }
     draw(){
         this.drawMain();
         this.drawBack();
+        this.drawItem();
     }
     drawMain(){
         goGame.context.fillStyle = this.color;
@@ -35,6 +42,26 @@ class ShopWindow{
             this.back.width,
             this.back.height
         );
+    }
+    drawItem(){
+        goGame.context.fillStyle = 'gray';
+        goGame.context.fillRect(
+            this.item.x,
+            this.item.y,
+            this.item.width,
+            this.item.height
+        );
+        // настройки текста
+        goGame.context.font = 'bold 20px courier';
+        goGame.context.textAlign = 'left';
+        goGame.context.textBaseline = 'top';
+        goGame.context.fillStyle = 'black';
+        goGame.context.fillText(
+            `Черви`,
+            this.item.x,
+            this.item.y + this.item.height
+        );
+
     }
     click(poE){
         if (
