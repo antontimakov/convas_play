85ee9a3fbe71dc2ea50eff7f5ee5253705d9b7ee
АМ <test@test.com>
2020-02-08 20:55:00 +0300

    добавлен клас Scene

---
+++ classes/Scene.js
@@ -0,0 +1,35 @@
+// класс определяющий параметры основнойсцены
+class Scene{
+    constructor() {
+        this.showed = false;
+    }
+    draw(){
+        if(this.showed){
+            for (let item in this.gaObjs){
+                if (this.gaObjs[item].draw){
+                    this.gaObjs[item].draw();
+                }
+            }
+        }
+    }
+    click(poE){
+        for (let item in this.gaObjs){
+            if (this.gaObjs[item].click){
+                this.gaObjs[item].click(poE);
+            }
+        }
+    }
+    over(poE){
+        for (let item in this.gaObjs){
+            if (this.gaObjs[item].over){
+                this.gaObjs[item].over(poE);
+            }
+        }
+    }
+    show(){
+        let me = this;
+        this.showed = true;
+        goGame.canvas.onclick = (poE)=>{me.click(poE)};
+        goGame.canvas.onmousemove = (poE)=>{me.over(poE)};
+    }
+}
--- classes/mainScene/MainScene.js
+++ classes/mainScene/MainScene.js
@@ -1,6 +1,7 @@
 // класс определяющий параметры основнойсцены
-class MainScene {
+class MainScene extends Scene{
     constructor() {
+        super();
         this.showed = true;
         this.gaObjs = {
             lake: new Lake(),
@@ -12,34 +13,5 @@ class MainScene {
             shop: new Shop(),
             fishMarket: new FishMarket()
         };
-    }
-    draw(){
-        if(this.showed){
-            for (let item in this.gaObjs){
-                if (this.gaObjs[item].draw){
-                    this.gaObjs[item].draw();
-                }
-            }
-        }
-    }
-    click(poE){
-        for (let item in this.gaObjs){
-            if (this.gaObjs[item].click){
-                this.gaObjs[item].click(poE);
-            }
-        }
-    }
-    over(poE){
-        for (let item in this.gaObjs){
-            if (this.gaObjs[item].over){
-                this.gaObjs[item].over(poE);
-            }
-        }
-    }
-    show(){
-        let me = this;
-        this.showed = true;
-        goGame.canvas.onclick = (poE)=>{me.click(poE)};
-        goGame.canvas.onmousemove = (poE)=>{me.over(poE)};
     }
 }
