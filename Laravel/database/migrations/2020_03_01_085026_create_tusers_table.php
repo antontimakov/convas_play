4db84bc2ab78fdc3780b66d26c0e5303ed5a58d0
АМ <test@test.com>
2020-03-01 13:08:34 +0300

    комент для создания

--- Laravel/database/migrations/2020_03_01_085026_create_tusers_table.php
+++ Laravel/database/migrations/2020_03_01_085026_create_tusers_table.php
@@ -1,5 +1,5 @@
 <?php
-
+// php artisan make:migration create_tusers_table --create=tusers
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
