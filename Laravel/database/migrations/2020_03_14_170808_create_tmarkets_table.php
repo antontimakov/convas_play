325243e16d833a093ee0ba5ab4eb9623d562c9fd
АМ <test@test.com>
2020-03-14 20:25:27 +0300

    migration tmarkets

---
+++ Laravel/database/migrations/2020_03_14_170808_create_tmarkets_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTmarketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tmarkets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('tuser_id')->comment('Пользователь');
+            $table->unsignedBigInteger('titem_id')->comment('Предмет');
+            $table->integer('bcount')->default(0)->comment('Кол-во');
+            $table->timestamp('time_sale')->comment('Время продажи');
+            $table->timestamps();
+
+            $table->foreign('tuser_id')->references('id')
+                ->on('tusers')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+
+            $table->foreign('titem_id')->references('id')
+                ->on('titems')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tmarkets');
+    }
+}
