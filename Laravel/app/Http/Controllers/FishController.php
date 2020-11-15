ecb3b9cd300541f0ee9ccfcfd9462cf3d7fd35a7
АМ <test@test.com>
2020-11-15 13:53:35 +0300

    getFish. Основной функционал

--- Laravel/app/Http/Controllers/FishController.php
+++ Laravel/app/Http/Controllers/FishController.php
@@ -1,31 +1,43 @@
 <?php
-//http://localhost/Laravel/public/getbagitems
 namespace App\Http\Controllers;
 
 use App\Titem;
+use App\Tbag;
+use App\Tuser;
 
 class FishController extends Controller
 {
     public function getFish()
     {
-        $laItems = $this->selAllItems();
-        $randItem = $this->idByRand($laItems['items'], $laItems['sumProbability']);
-        print_r($randItem);
+        $laItems = $this->getAllItems();
+        $item = $this->randItem($laItems['items'], $laItems['sumProbability']);
+        $itemId = $item['itemId'];
+        $countItems = $this->getCountItemsById($itemId);
+        if ($countItems === 0) {
+            $this->createNewRecTbag($itemId);
+        }
+        $this->incrementItemById($itemId);
+        $this->addExperience($item['experience']);
+        return $item;
     }
 
     /**
      * Выбирает категорию в соответстви с распределением вероятностей
      * @param array $paRes массив из базы
      * @param int $sumProbability
-     * @return int|null
+     * @return array|null
      */
-    protected function idByRand(array $paItems, int $sumProbability){
-        $lnRand = mt_rand (0, $sumProbability - 1);
+    protected function randItem(array $paItems, int $sumProbability): array
+    {
+        $lnRand = mt_rand(0, $sumProbability - 1);
         $lnSumProb = 0; // сумма вероятностей
         foreach ($paItems as $paItem) {
             $lnSumProb += $paItem['probability'];
-            if ($lnSumProb > $lnRand){
-                return (int)$paItem['id'];
+            if ($lnSumProb > $lnRand) {
+                return [
+                    'itemId' => $paItem['id'],
+                    'experience' => $paItem['experience']
+                ];
             }
         }
         return null;
@@ -35,11 +47,12 @@ class FishController extends Controller
      * Выбирает предметы которые можем выловить и "вероятности" их улова
      * @return array Массив предметов и сумма "Вероятностей"
      */
-    protected function selAllItems(){
+    protected function getAllItems(): array
+    {
         $res = Titem::get();
         $items = [];
         $sumProbability = 0;
-        foreach($res as $r){
+        foreach ($res as $r) {
             $probability = $r
                 ->titemTypes
                 ->tprior
@@ -47,12 +60,64 @@ class FishController extends Controller
             $sumProbability += $probability;
             $items[] = [
                 'id' => $r->id,
-                'probability' => $probability
+                'probability' => (int)$probability,
+                'experience' => (int)$r->experience
             ];
         }
         return [
             'items' => $items,
             'sumProbability' => $sumProbability
         ];
+    }
+
+    /**
+     * Возвращает кол-во предметов по идентификатору
+     * @param int $itemId Идентификатор предмета
+     * @return int Кол-во предматов
+     */
+    protected function getCountItemsById(int $itemId): int
+    {
+        return (int)Tbag::where([
+                ['tuser_id', '=', 1],
+                ['titem_id', '=', $itemId]
+            ])
+            ->count();
+    }
+
+    /**
+     * Создаёт запись в таблице рюкзака
+     * @param int $titemId Идентификатор новой записи
+     */
+    protected function createNewRecTbag(int $titemId): void
+    {
+        $tbag = new Tbag;
+        $tbag->tuser_id = 1;
+        $tbag->titem_id = $titemId;
+        $tbag->save();
+    }
+
+    /**
+     * Увеличиывет кол-во выловленных предметов
+     * @param int $itemId Идентификатор предмета
+     */
+    protected function incrementItemById(int $itemId): void
+    {
+        $tbag = Tbag::where([
+            ['titem_id', '=', $itemId],
+            ['tuser_id', '=', 1]
+        ])->first();
+        $tbag->bcount = $tbag->bcount + 1;
+        $tbag->save();
+    }
+
+    /**
+     * Добавляет опыт пользователю
+     * @param int $experience Кол-во опыта
+     */
+    protected function addExperience(int $experience): void
+    {
+        $tuser = Tuser::find(1);
+        $tuser->experience += $experience;
+        $tuser->save();
     }
 }
