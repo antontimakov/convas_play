aa2b78625c3c4bd3343419a4722148853c8a3216
timakov <atimakov@srv.rbt>
2020-12-17 16:06:15 +0300

    class 1
    construct 2
    
    getGish 3

--- Laravel/app/Http/Controllers/FishController.php
+++ Laravel/app/Http/Controllers/FishController.php
@@ -6,16 +6,27 @@ namespace App\Http\Controllers;
 
 use App\Services\Main\FishService;
 
+/**
+ * Class FishController
+ * @package App\Http\Controllers
+ */
 class FishController extends Controller
 {
     protected FishService $fishService;
 
+    /**
+     * FishController constructor.
+     * @param FishService $fishService
+     */
     public function __construct(
         FishService $fishService
     ) {
         $this -> fishService = $fishService;
     }
 
+    /**
+     * @return array
+     */
     public function getFish(): array
     {
         return $this->fishService->index();
